3a239e2fa22764e1e9e403030a9c47fcd9a0cc0c
meticulousfan <elithofan@gmail.com>
2022-08-03 18:52:02 +0900

    renamed: contextwrapper page

---
+++ components/Layout/ContextWrapper.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { ApolloProvider, useQuery, gql } from "@apollo/client";
+import { AppProps } from "next/app";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+import Layout from "./Layout";
+import { token } from "../../lib/apollo";
+
+export const UserContext = React.createContext({
+  currentUser: null,
+  setCurrentUser: (p: object | null): void => {}
+});
+
+const GET_PROFILE = gql`
+  query GetProfile {
+    getProfileByToken {
+      id
+      name
+      email
+      role
+    }
+  }
+`;
+
+const ContextWrapper: NextPage<AppProps> = (props) => {
+  const { Component, pageProps } = props;
+  const router = useRouter();
+  const [currentUser, setCurrentUser] = useState(null);
+  const [localToken, setToken] = useState(token);
+  const { data, loading, error, refetch } = useQuery(GET_PROFILE);
+  useEffect(() => {
+    if (localToken) {
+      if (data && data?.getProfileByToken) {
+        setCurrentUser(data?.getProfileByToken);
+      }
+      if (error) router.push("/account/signin");
+    }
+  }, [data, error, currentUser]);
+
+  const value = { currentUser, setCurrentUser };
+  return (
+    <UserContext.Provider value={value}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserContext.Provider>
+  );
+};
+
+export default ContextWrapper;
