2d35fcea6b537a048a1af9289fd22fadb79b3fa1
meticulousfan <elithofan@gmail.com>
2022-08-03 18:51:03 +0900

    fixed: _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { ApolloProvider } from "@apollo/client";
 import apolloClient from "../lib/apollo";
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
-import ContextWrapper from "../components/Layout/Layout";
+import ContextWrapper from "../components/Layout/ContextWrapper";
 import Modal from "react-modal";
 
 Modal.setAppElement("body");
