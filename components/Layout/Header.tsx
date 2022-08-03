9e8ca786914a61eace5f70463ef99ea8d795607d
meticulousfan <elithofan@gmail.com>
2022-08-03 18:51:45 +0900

    added: login, register links

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { UserContext } from "../Layout/Layout";
+import { UserContext } from "./ContextWrapper";
 
 const Header = () => {
   const router = useRouter();
@@ -17,48 +17,64 @@ const Header = () => {
 
   return (
     <header>
-      {currentUser && (
-        <div className="px-4 py-2 text-white flex  justify-between bg-blue-900">
-          <h1>LOGO</h1>
-          <div
-            className={
-              toggleMenu
-                ? "md:flex  md:pt-0 pt-10 w-full md:w-auto"
-                : "hidden md:flex"
-            }
-            id="menu"
-          >
-            <ul>
-              <li className="md:inline-block cursor-pointer hover:text-gray-500 border-b md:border-none py-2 px-3">
-                <Link href="/blogs">Blogs</Link>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <span className="py-2 px-2 md:border-none">
+      <div className="px-4 py-2 text-white flex  justify-between bg-blue-900">
+        <h1>LOGO</h1>
+        <div
+          className={
+            toggleMenu
+              ? "md:flex  md:pt-0 pt-10 w-full md:w-auto"
+              : "hidden md:flex"
+          }
+          id="menu"
+        >
+          <ul>
+            <li className="md:inline-block cursor-pointer hover:text-gray-500 border-b md:border-none py-2 px-3">
+              <Link href="/blogs">Blogs</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          {currentUser && (
+            <span className="py-2 px-2 md:border-none" data-cy="username">
               {currentUser?.name}
             </span>
+          )}
+          {currentUser ? (
             <button
               className="py-2 md:border-none bg-gray-400 text-color-dark rounded"
+              data-cy="logout"
               onClick={() => logOut()}
             >
               logout
             </button>
-          </div>
-          <div className="cursor-pointer md:hidden">
-            <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
-            <label
-              className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none"
-              htmlFor="menu-btn"
-            >
-              <span
-                onClick={handleToggle}
-                className="navicon bg-white-darkest flex items-center relative"
-              ></span>
-            </label>
-          </div>
+          ) : (
+            <>
+              <Link href="/account/signin">
+                <span className="py-2 md:border-none cursor-pointer hover:text-gray-500">
+                  Login
+                </span>
+              </Link>
+              <Link href="/account/signup">
+                <span className="py-2 px-2 md:border-none cursor-pointer hover:text-gray-500">
+                  Register
+                </span>
+              </Link>
+            </>
+          )}
         </div>
-      )}
+        <div className="cursor-pointer md:hidden">
+          <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
+          <label
+            className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none"
+            htmlFor="menu-btn"
+          >
+            <span
+              onClick={handleToggle}
+              className="navicon bg-white-darkest flex items-center relative"
+            ></span>
+          </label>
+        </div>
+      </div>
     </header>
   );
 };
